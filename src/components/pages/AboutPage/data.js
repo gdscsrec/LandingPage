70ba1cae40a3e85e2b60d6032cb54324e55dc8a5
Nandy <nandhinilingaraj199@gmail.com>
2021-12-31 21:49:26 +0530

    done

--- src/components/pages/AboutPage/data.js
+++ src/components/pages/AboutPage/data.js
@@ -20,7 +20,7 @@ export const ObjTwo = {
   lightTopLine: false,
   lightTextDesc: false,
   description:
-    "The main scope of GDSC SREC is to bring out the best in people by giving enough knowledge and training about a particular domain. Solving technical ideas, managing events, projects, public relations among social media and designing themes for GDSC. These are the core jobs done by our community. We feel happy and pride on motivating others to successfully project their ideas in a better way!!The main scope of GDSC SREC is to bring out the best in people by giving enough knowledge and training about a particular domain. Solving technical ideas, managing events, projects, public relations among social media and designing themes for GDSC. These are the core jobs done by our community. We feel happy and pride on motivating others to successfully project their ideas in a better way!!",
+    "The main scope of GDSC SREC is to bring out the best in people by giving enough knowledge and training about a particular domain. Solving technical ideas, managing events, projects, public relations among social media and designing themes for GDSC. These are the core jobs done by our community. We feel happy and pride on motivating others to successfully project their ideas in a better way!!",
   headline: "Why are we here?",
   lightText: true,
   img: require("../../images/logo.webp").default,
